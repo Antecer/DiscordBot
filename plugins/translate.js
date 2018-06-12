b4807866606ffef25d8d80eaa6f9dd43b54c6bae
Antecer <Antecer@outlook.com>
2018-06-12 20:14:50 +0800

    edit translate plugin

--- plugins/translate.js
+++ plugins/translate.js
@@ -3,6 +3,12 @@ const http = require('http');
 
 module.exports.run = async (bot, message) => {
     //"http://translate.google.com/translate_a/single?client=gtx&sl=en&tl=zh-CN&dt=t&q=test"
+    let info = new Discord.RichEmbed()
+        .setAuthor(message.member.nickname, message.author.avatarURL)
+        .setTitle("Google Translate To:zh-CN")
+        .setDescription("translate...");
+    let msg = await message.channel.send(info);
+
     let options = {
         hostname: 'translate.google.com',
         port: 80,
@@ -21,19 +27,28 @@ module.exports.run = async (bot, message) => {
             Tresult[0].forEach(e => {
                 chinese += e[0];
             });
-            let info = new Discord.RichEmbed()
-                .setAuthor(message.member.nickname, message.author.avatarURL)
-                .setTitle("Google Translate")
-                .addField("To Chinese:", chinese);
-            let msg = await message.channel.send(info);
+            info.setAuthor(message.member.nickname, message.author.avatarURL)
+                .setTitle("Google Translate To:zh-CN")
+                .setDescription(chinese);
+            msg.edit(info);
             setTimeout(() => {
                 msg.delete();
             }, 5000)
         });
         res.on("error", (e) => {
+            info.setDescription(`Translate Failed:\r\n${e.message}`);
+            msg.edit(info);
+            setTimeout(() => {
+                msg.delete();
+            }, 5000)
             console.error(e.message);
         });
     }).on('error', (e) => {
+        info.setDescription(`Translate Failed:\r\n${e.message}`);
+        msg.edit(info);
+        setTimeout(() => {
+            msg.delete();
+        }, 5000)
         console.error(e.message);
     }).end();
 };
