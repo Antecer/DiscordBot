55053428c2238752cfd52cea87253517b1e0d213
Antecer <Antecer@outlook.com>
2018-06-14 12:30:31 +0800

    add translate-to-english

--- plugins/translate.js
+++ plugins/translate.js
@@ -3,68 +3,102 @@ const http = require('http');
 
 module.exports.run = async (bot, message) => {
     let translateChannels = ["translate-to-chinese", "translate-to-english"];
-
-    let info = new Discord.RichEmbed()
-        .setAuthor(message.author.username, message.author.avatarURL)
-        .setTitle("Google Translate To:zh-CN")
-        .setDescription("translate...");
-    let msg;
-    if(message.member)
-    {
+    if(message.member){ // 创建翻译频道
         translateChannels.forEach(cname => {
-            if(!message.guild.channels.find(c => c.name === cname))
-            {
+            if(!message.guild.channels.find(c => c.name === cname)){
                 message.guild.createChannel(cname, 'text')
                     .then(console.log(`[info]创建频道: ${cname}`))
                     .catch(console.error);
             }
         });
-        if(message.member.nickname != null){
-            info.setAuthor(message.member.nickname, message.author.avatarURL);
-        }
-        msg = await message.guild.channels.find(c => c.name === `translate-to-chinese`).send(info);
     }
-    else
+    let authorname = message.member.nickname == "null" ? message.author.username : message.member.nickname;
+
+    if(message.channel.name != "translate-to-chinese")
     {
-        msg = await message.channel.send(info);
-    }
-    
-    let options = {
-        hostname: 'translate.google.com',
-        port: 80,
-        path: encodeURI(`/translate_a/single?client=gtx&sl=auto&tl=zh-CN&dt=t&q=${message.content}`),
-        method: 'GET'
-    }
-    new Promise(function (resolve, reject) {
-        let req = http.request(options, function(res) {
-            let html = "";
-            res.on('data', chunk => {
-                html += chunk;
+        let info = new Discord.RichEmbed()
+            .setAuthor(authorname, message.author.avatarURL)
+            .setTitle("Google Translate To:zh-CN")
+            .setDescription("translate...");
+        let msg = await message.guild.channels.find(c => c.name === `translate-to-chinese`).send(info);
+        let options = {
+            hostname: 'translate.google.com',
+            port: 80,
+            path: encodeURI(`/translate_a/single?client=gtx&sl=auto&tl=zh-CN&dt=t&q=${message.content}`),
+            method: 'GET'
+        }
+        new Promise(function (resolve, reject) {
+            let req = http.request(options, function(res) {
+                let html = "";
+                res.on('data', chunk => {
+                    html += chunk;
+                });
+                res.on('end', () => {
+                    resolve(html);
+                });
+                res.on("error", (error) => {
+                    reject(error);
+                });
             });
-            res.on('end', () => {
-                resolve(html);
-            });
-            res.on("error", (error) => {
+            req.on('error', (error) => {
                 reject(error);
             });
+            req.end();
+        })
+        .then(html =>{
+            let data = "";
+            JSON.parse(html)[0].forEach(t => { data += t[0]; });
+            info.setDescription(data);
+            msg.edit(info);
+        })
+        .catch(error => {
+            info.setDescription(`Translate Failed:\r\n${error.message}`);
+            msg.edit(info);
         });
-        req.on('error', (error) => {
-            reject(error);
+    }
+
+    if(message.channel.name != "translate-to-english")
+    {
+        let info = new Discord.RichEmbed()
+            .setAuthor(authorname, message.author.avatarURL)
+            .setTitle("Google Translate To:en")
+            .setDescription("translate...");
+        let msg = await message.guild.channels.find(c => c.name === `translate-to-english`).send(info);
+        let options = {
+            hostname: 'translate.google.com',
+            port: 80,
+            path: encodeURI(`/translate_a/single?client=gtx&sl=auto&tl=en&dt=t&q=${message.content}`),
+            method: 'GET'
+        }
+        new Promise(function (resolve, reject) {
+            let req = http.request(options, function(res) {
+                let html = "";
+                res.on('data', chunk => {
+                    html += chunk;
+                });
+                res.on('end', () => {
+                    resolve(html);
+                });
+                res.on("error", (error) => {
+                    reject(error);
+                });
+            });
+            req.on('error', (error) => {
+                reject(error);
+            });
+            req.end();
+        })
+        .then(html =>{
+            let data = "";
+            JSON.parse(html)[0].forEach(t => { data += t[0]; });
+            info.setDescription(data);
+            msg.edit(info);
+        })
+        .catch(error => {
+            info.setDescription(`Translate Failed:\r\n${error.message}`);
+            msg.edit(info);
         });
-        req.end();
-    })
-    .then(html =>{
-        let chinese = "";
-        JSON.parse(html)[0].forEach(t => { chinese += t[0]; });
-        info.setAuthor(message.member.nickname, message.author.avatarURL)
-            .setTitle("Google Translate To:zh-CN")
-            .setDescription(chinese);
-        msg.edit(info);
-    })
-    .catch(error => {
-        info.setDescription(`Translate Failed:\r\n${error.message}`);
-        msg.edit(info);
-    });
+    }
 };
 
 module.exports.help = {
