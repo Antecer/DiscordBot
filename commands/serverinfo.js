1290352c1825d180d6a888b493359d8d8aff1539
Antecer <Antecer@outlook.com>
2018-07-08 18:48:42 +0800

    fixed !serverinfo username show null.

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -2,15 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let msg = await message.channel.send("Query running, please wait..");
+    let username = message.member.nickname ? message.member.nickname : message.author.username;
 
     let serverinfo = new Discord.RichEmbed()
         .setColor("#A0A0A0")
         .setAuthor(message.guild.name, message.guild.iconURL, message.guild.iconURL)
         .addField("Server Created On:", message.guild.createdAt)
-        .addField(`${message.member.nickname} Joined At:`, message.member.joinedAt)
+        .addField(`${username} Joined At:`, message.member.joinedAt)
         .addField("Total Members:", message.guild.memberCount)
         ;
-    await message.channel.send({embed: serverinfo});
+    await message.channel.send(serverinfo);
 
     msg.delete();
 }
